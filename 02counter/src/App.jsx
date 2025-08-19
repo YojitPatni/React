82de3efaff95099aaac721a4a0b4abfc89ff6921
Yojithello <yojit0923.be23@chitkara.edu.in>
2025-08-19 17:11:34 +0530

    Initialize 02counter project with React and Vite setup, including essential files and configurations. Implement counter functionality in App component with add and remove features. Add styles for layout and components, and include SVG assets for logos.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(5)
+  
+  //let counter=5
+
+  const addValue=()=>{
+    console.log('Add value : ', counter);
+    //counter++;
+    setCounter(counter+1) // or just put counter 
+  }
+
+  const removeValue=()=>{
+    setCounter(counter-1);
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value: {counter}</button><br />
+      <button onClick={removeValue} >Remove value</button>
+    </>
+  )
+}
+
+export default App
