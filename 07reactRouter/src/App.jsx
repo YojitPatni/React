fa360c6453efb1d077f8fcc235718fbf59c0be46
Yojithello <yojit0923.be23@chitkara.edu.in>
2025-10-08 00:57:20 +0530

    Reacr router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header/Header.jsx'
+import Home from './components/Home/Home.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
